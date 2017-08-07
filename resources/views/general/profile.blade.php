b03a56658dd33912a8c72ce353238b3000f09c8f
elderjr <elderjr1995@gmail.com>
2017-08-07 10:57:35 -0300

    mobile - profile

--- resources/views/general/profile.blade.php
+++ resources/views/general/profile.blade.php
@@ -6,7 +6,7 @@
     </div>
 </div>
 <div class="row">
-    <div class="col-md-6 col-sm-6 col-xs-6">
+    <div class="col-md-6 col-sm-6 col-xs-12">
         <div class="x_panel">
             <div class="x_title">
                 <h2>Informaçoes Gerais</h2>
@@ -15,21 +15,21 @@
             <div class="x_content">
                 <form class="form-vertical form-label-left" action="{{action("GeneralController@profile")}}" method="get">
                     <div class="row">
-                        <div class="col-md-12">
+                        <div class="col-md-12 col-sm-12 col-xs-12">
                             <label>Nome:</label>
                             <input type="text" id="name" name="name" class="form-control " value="{{$generalInformation->user->name}}">
                         </div>
                     </div>
                     <br/>
                     <div class="row">
-                        <div class="col-md-12">
+                        <div class="col-md-12 col-sm-12 col-xs-12">
                             <label class="control-label">E-mail</label>
                             <input type="text" id="email" name="email" class="form-control" value="{{$generalInformation->user->email}}">
                         </div>
                     </div>
                     <div class="ln_solid"></div>
                     <div class="row">
-                        <div class="col-md-5 col-md-offset-7">
+                        <div class="col-md-5 col-md-offset-7 col-xs-6 col-xs-offset-6">
                             <button type="submit" class="btn btn-success btn-block">Confirmar</button>
                         </div>
                     </div>
@@ -37,7 +37,7 @@
             </div>
         </div>
     </div>
-    <div class="col-md-6 col-sm-6 col-xs-6">
+    <div class="col-md-6 col-sm-6 col-xs-12">
         <div class="x_panel">
             <div class="x_title">
                 <h2>Alterar Senha</h2>
@@ -46,25 +46,25 @@
             <div class="x_content">
                 <form class="form-vertical form-label-left">
                     <div class="row">
-                        <div class="col-md-12">
+                        <div class="col-md-12 col-sm-12 col-xs-12">
                             <label class="control-label">Senha Atual:</label>
                             <input type="password" id="oldPassword" name="oldPassword" class="form-control">
                         </div>
                     </div>
                     <br/>
                     <div class="row">
-                        <div class="col-md-6">
+                        <div class="col-md-6 col-sm-6 col-xs-12">
                             <label class="control-label">Nova Senha</label>
                             <input type="password" id="newPassword1" name="newPassword1" class="form-control" />
                         </div>
-                        <div class="col-md-6">
+                        <div class="col-md-6 col-sm-6 col-xs-12">
                             <label class="control-label">Confirmação</label>
                             <input type="password" id="newPassword2" name="newPassword2" name="email" class="form-control " />
                         </div>
                     </div>
                     <div class="ln_solid"></div>
                     <div class="row">
-                        <div class="col-md-5 col-md-offset-7">
+                        <div class="col-md-5 col-md-offset-7 col-xs-6 col-xs-offset-6">
                             <button type="submit" class="btn btn-success btn-block">Alterar Senha</button>
                         </div>
                     </div>
